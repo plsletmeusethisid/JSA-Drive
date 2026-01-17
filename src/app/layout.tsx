ad822ccbaa9074c0b800cdd9115aadf5eb1545a4
plsletmeusethisid <luykim427@gmail.com>
2026-01-17 12:27:49 +0900

    jsa-drive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { Toaster } from "@/components/ui/sonner";
+import "@liveblocks/react-ui/styles.css";
+import "@liveblocks/react-tiptap/styles.css";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -26,9 +25,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
-        {children}
+        <NuqsAdapter>
+          <ConvexClientProvider>
+            <Toaster />
+            {children}
+          </ConvexClientProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
---
+++ src/constants/templates.ts
@@ -0,0 +1,49 @@
+export const templates = [
+    { 
+        id: "blank", 
+        label: "Blank Document", 
+        imageUrl: "/blank-document.svg", 
+    },
+    { 
+        id: "Daily Battle Summary", 
+        label: "Daily Battle Summary", 
+        imageUrl: "/blank-document.svg",
+        initialContent: `
+            <p>This page was meant to be left blank for security reasons</p>
+        ` 
+    },
+    { 
+        id: "Weekly Battle Schedule", 
+        label: "Weekly Battle Schedule", 
+        imageUrl: "/blank-document.svg",
+        initialContent: `
+            <p>This page was meant to be left blank for security reasons</p>
+        ` 
+    },
+    { 
+        id: "Monthly Battalion Schedule", 
+        label: "Monthly Battalion Schedule", 
+        imageUrl: "/blank-document.svg",
+        initialContent: `
+            <p>This page was meant to be left blank for security reasons</p>
+        ` 
+    },
+    { 
+        id: "Daily Departure Status", 
+        label: "Daily Departure Status", 
+        imageUrl: "/blank-document.svg",
+        initialContent: `
+            <p>This page was meant to be left blank for security reasons</p>
+        ` 
+    },
+    { 
+        id: "project-proposal", 
+        label: "Project proposal", 
+        imageUrl: "/project-proposal.svg",
+    },
+    { 
+        id: "letter", 
+        label: "Letter", 
+        imageUrl: "/letter.svg" 
+    },
+];
